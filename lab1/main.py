091e6a3984fd6d409b39934ee8ca097e95459c29
Kyrollos Bekhet <kyrollos.bekhet@gmail.com>
2022-10-01 19:45:04 -0400

    Load balancer and Target groups creation
    In this commit, the necessary code needed to create Load balancers and
    Target groups was added in ELB_setup.py file. The main.py is used to
    test the functions created.
    
    Signed-off-by: Kyrollos Bekhet <kyrollos.bekhet@gmail.com>

--- lab1/main.py
+++ lab1/main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from ELB_setup import *
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+""" 
+TODO: Remove the main function the goal of this method is to test the creation of the load balancer
+ and target groups. Also to identify what is needed to create the load balancer
+"""
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    cluster1 = create_target_groups("cluster1")["TargetGroups"][0]
+    cluster2 = create_target_groups("cluster2")["TargetGroups"][0]
+    target_groups = [cluster1['TargetGroupArn'], cluster2['TargetGroupArn']]
+    # Hardcoded security group id
+    subnets = ['subnet-0368628d6c8694be6', 'subnet-0584deae6391f04bf']
+    security_groups = ['sg-03d6e26eae0579bd9']
+    create_load_balancer(subnets, security_groups, target_groups)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+main()

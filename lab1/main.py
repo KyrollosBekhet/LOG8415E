c5c14f728e8b813b67a4e1c185bfc95e2eff3974
Philippe-Savard <philippe-1.savard@polymtl.ca>
2022-10-05 20:15:46 -0400

    Added boto3 sessions to main.py

--- lab1/main.py
+++ lab1/main.py
@@ -13,9 +13,13 @@ TODO: Remove the main function the goal of this method is to test the creation o
 
 
 def main():
-    elb_client = boto3.client('elbv2')
-    ec2_client = boto3.client('ec2')
-    ec2_resource = boto3.resource('ec2')
+    # Creating session using default credentials and configuration.
+    session = boto3.Session(profile_name='default')
+
+    elb_client = session.client('elbv2')
+    ec2_client = session.client('ec2')
+    ec2_resource = session.resource('ec2')
+
     default_security_group = ec2_client.describe_security_groups(GroupNames=['default'])['SecurityGroups'][0]
     vpcs = ec2_client.describe_vpcs()
     vpc_id = vpcs.get('Vpcs', [{}])[0].get('VpcId', '')
@@ -47,8 +51,12 @@ def main():
     # for now number of instances are 1
     awake_instances = None
     while awake is False:
+        # Name filter used to prevent existing instances from interfering 
         awake_instances = ec2_resource.instances.filter(
-            Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]
+            Filters=[
+                {'Name': 'instance-state-name', 'Values': ['running']}, 
+                {'Name': 'tag:Name', 'Values': ['cluster1', 'cluster2']}
+            ]
         )
         if len(list(awake_instances.all())) == 1:
             awake = True
@@ -56,10 +64,11 @@ def main():
     cluster1_instances = awake_instances.filter(
         Filters=[{'Name': 'tag:Name', 'Values': ['cluster1']}]
     )
+
     cluster1_targets_ids = []
     for ins in cluster1_instances.all():
         cluster1_targets_ids.append({'Id': ins.id})
-
+    
     add_instance_to_target_group(elb_client, target_groups[0], cluster1_targets_ids)
     security_groups = [sg['GroupId']]
 
@@ -92,7 +101,6 @@ def main():
                 if terminated_ins.id.__eq__(created_instance.id):
                     awake_vms_number -= 1
                     print('awake_vm decreased')
-
         if awake_vms_number == 0:
             awake = False
     """

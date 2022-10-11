877b1cfd758c5ce76f0c83f25e55dfb2f6486452
David Piche <david.piche@polymtl.ca>
2022-10-11 16:29:16 -0400

    Encapsulated in getCpuUsage function with arguments
    Signed-off-by: David Piche <david.piche@polymtl.ca>

--- lab1/get_metrics.py
+++ lab1/get_metrics.py
@@ -6,25 +6,29 @@ import boto3
 
 client = boto3.client('cloudwatch', region_name='us-west-2')
 
-response = client.get_metric_data(
-    MetricDataQueries=[
-        {
-            'Id': 'cpu_usage'
-            'MetricStat': {
-                'Metric': {
-                        'Namespace': 'AWS/EC2',
-                        'MetricName': 'CPUUtilization'          
+
+
+def getCpuUsage(client, period, time):
+
+    response = client.get_metric_data(
+        MetricDataQueries=[
+            {
+                'Id': 'cpu_usage'
+                'MetricStat': {
+                    'Metric': {
+                            'Namespace': 'AWS/EC2',
+                            'MetricName': 'CPUUtilization'          
+                    },
+                    'Period': period,
+                    'Stat': 'Avg',
+                    'Unit': 'Percent'
                 },
-                'Period': 15,
-                'Stat': 'Avg',
-                'Unit': 'Percent'
-            },
-            'Period': 15, # Requests interval
+                'Period': period, # Requests interval
+            }
+        ],
+        StartTime=datetime.utcnow()-timedelta(seconds=time), # Requests metrics from 10 minutes ago
+        EndTime=datetime.utcnow(),
+        LabelOptions={
+            'Timezone': -0400 # Timezone offset from UTC for Eastern Time
         }
-    ],
-    StartTime=datetime.utcnow()-timedelta(seconds=600), # Requests metrics from 10 minutes ago
-    EndTime=datetime.utcnow(),
-    LabelOptions={
-        'Timezone': -0400 # Timezone offset from UTC for Eastern Time
-    }
-)
+    )

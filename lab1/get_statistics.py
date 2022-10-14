8eb81da0d8482ff134e5bcd2cfad0d0ea9b7a869
David Piche <david.piche@polymtl.ca>
2022-10-14 12:49:32 -0400

    Added get_statistics skeleton with printPlot method
    Signed-off-by: David Piche <david.piche@polymtl.ca>

---
+++ lab1/get_statistics.py
@@ -0,0 +1,35 @@
+import boto3
+import matplotlib.pyplot as plt
+import numpy as np
+from get_metrics.py import getMetric
+
+
+
+client = boto3.client('cloudwatch', region_name='us-east-2')
+
+# Wait some time
+
+
+rep = getMetric(client,'cpu_usage','AWS/EC2','CPUUtilization',15,'Sum','Percent',600)
+
+print(rep['Messages'])
+
+resp = rep['MetricDataResults'][0]
+
+
+printPlot(resp)
+
+def printPlot(resp):
+
+x = np.array(resp['Values'][0])
+y = np.array(resp['Timestamp'][0])
+
+
+for n in range(1,len(resp['Values'])):
+
+    x = np.append(x,resp['Values'][n],axis=0)
+    y = np.append(x,resp['Timestamp'][n],axis=0)
+
+plt.scatter(x, y)
+plt.show()
+

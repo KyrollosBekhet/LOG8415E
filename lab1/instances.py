3225261c3444b7da069fd0a6ca168c597112d35b
Philippe-Savard <65316883+Philippe-Savard@users.noreply.github.com>
2022-10-16 10:28:08 -0400

    Removed useless comments and formatted instances.py

--- lab1/instances.py
+++ lab1/instances.py
@@ -1,8 +1,7 @@
 from threading import Thread
 
 
-def create_key_pair(ec2_resource,
-                    key_name):
+def create_key_pair(ec2_resource, key_name):
     """
     Creates a key pair to securely connect to the AWS instances
     :param key_name: The name of the key pair
@@ -12,9 +11,7 @@ def create_key_pair(ec2_resource,
     return ec2_resource.create_key_pair(KeyName=key_name)
 
 
-def create_instances(ec2_resource, image_id, instance_type, key_name, tags,
-                     subnet_id, count,
-                     security_group_id):
+def create_instances(ec2_resource, image_id, instance_type, key_name, tags, subnet_id, count, security_group_id):
     tag_spec = [
         {
             'ResourceType': 'instance',
@@ -32,26 +29,31 @@ def create_instances(ec2_resource, image_id, instance_type, key_name, tags,
         'SubnetId': subnet_id, 'TagSpecifications': tag_spec
     }
     instances = ec2_resource.create_instances(**instance_params, MinCount=count, MaxCount=count)
+
     print(instances)
 
 
-def terminate_instances(ec2_resource, instances_ids):
-    threads = []
-    for ins in instances_ids:
-        thread = Thread(target=do_terminate, args=[ec2_resource, ins['Id']])
-        thread.start()
-        threads.append(thread)
-
-    for thread in threads:
-        thread.join()
-        # instance = ec2_resource.Instance(ins['Id'])
-        #instance.terminate()
-        #instance.wait_until_terminated()
-
-    #print(ec2_client.terminate_instances(InstanceIds=ids))
-
-
 def do_terminate(ec2_resource, instance_id):
+    """
+    This function terminates an EC2 instance and wait for its state to be terminated.
+    :param instance_id: id of the instance to terminate. 
+    """
     instance = ec2_resource.Instance(instance_id)
     instance.terminate()
     instance.wait_until_terminated()
+
+
+def terminate_instances(ec2_resource, instances_ids):
+    """
+    This function terminates multiple instances simultaniously.
+    :param instances_ids: A list of instance id to terminate
+    """
+    threads = []
+    for instance in instances_ids:
+        thread = Thread(target=do_terminate, args=[ec2_resource, instance['Id']])
+        thread.start()
+        threads.append(thread)
+
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()

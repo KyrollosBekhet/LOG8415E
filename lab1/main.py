6ea217303914d8146bce407aa86bc58aa83847a9
Philippe-Savard <65316883+Philippe-Savard@users.noreply.github.com>
2022-10-16 02:36:09 -0400

    Removed useless comments and cleaned up main.py

--- lab1/main.py
+++ lab1/main.py
@@ -1,24 +1,18 @@
-from ipaddress import ip_address
-import time
-
 from ELB_setup import *
-import boto3
 from ELB_teardown import *
-from security_group import *
+from endpoint_call import *
 from instances import *
+from ipaddress import ip_address
+from nginx_file_writter import *
+from security_group import *
 from ssh_connection import *
 from threading import Thread
-from endpoint_call import *
 
-from nginx_file_writter import *
-
-""" 
-TODO: Remove the main function the goal of this method is to test the creation of the load balancer
- and target groups. Also to identify what is needed to create the load balancer
-"""
+import boto3
+import time
 
 
-def main():
+if __name__ == '__main__':
     # Creating session using default credentials and configuration.
     session = boto3.Session(profile_name='default')
 
@@ -28,11 +22,12 @@ def main():
 
     vpcs = ec2_client.describe_vpcs()
     vpc_id = vpcs.get('Vpcs', [{}])[0].get('VpcId', '')
+
     sg = create_security_group(ec2_client, vpc_id)
-    cluster1 = create_target_groups(elb_client, "cluster1", vpc_id)[
-        "TargetGroups"][0]
-    cluster2 = create_target_groups(elb_client, "cluster2", vpc_id)[
-        "TargetGroups"][0]
+
+    cluster1 = create_target_groups(elb_client, "cluster1", vpc_id)["TargetGroups"][0]
+    cluster2 = create_target_groups(elb_client, "cluster2", vpc_id)["TargetGroups"][0]
+
     target_groups = [cluster1['TargetGroupArn'], cluster2['TargetGroupArn']]
 
     security_groups = [sg['GroupId']]
@@ -40,12 +35,10 @@ def main():
     sn_all = ec2_client.describe_subnets()
     subnets = []
     for sn in sn_all['Subnets']:
-        if sn['AvailabilityZone'] == 'us-east-1a' or \
-                sn['AvailabilityZone'] == 'us-east-1b':
+        if sn['AvailabilityZone'] == 'us-east-1a' or sn['AvailabilityZone'] == 'us-east-1b':
             subnets.append(sn['SubnetId'])
 
-    load_balancer = create_load_balancer(elb_client, subnets,
-                                         security_groups, target_groups)
+    load_balancer = create_load_balancer(elb_client, subnets, security_groups, target_groups)
 
     write_file_content(load_balancer['LoadBalancerDNS'])
 
@@ -53,24 +46,21 @@ def main():
 
     private_key = ec2_client.describe_key_pairs()['KeyPairs'][0]
     try:
-        # cluster 1 instances
-        create_instances(ec2_resource, instances_ami, "t2.large",
-                         private_key["KeyName"], "cluster1", subnets[0], 3, sg['GroupId'])
-        create_instances(ec2_resource, instances_ami, "t2.large",
-                         private_key["KeyName"], "cluster1", subnets[1], 2, sg['GroupId'])
-        # cluster 2 instances
-        create_instances(ec2_resource, instances_ami, "m4.large",
-                         private_key["KeyName"], "cluster2", subnets[1], 2, sg['GroupId'])
-        create_instances(ec2_resource, instances_ami, "m4.large",
-                         private_key["KeyName"], "cluster2", subnets[0], 2, sg['GroupId'])
-        print("Instances created")
+        # Create cluster 1 instances
+        create_instances(ec2_resource, instances_ami, "t2.large", private_key["KeyName"], "cluster1", subnets[0], 3, sg['GroupId'])
+        create_instances(ec2_resource, instances_ami, "t2.large", private_key["KeyName"], "cluster1", subnets[1], 2, sg['GroupId'])
+
+        # Create cluster 2 instances
+        create_instances(ec2_resource, instances_ami, "m4.large", private_key["KeyName"], "cluster2", subnets[1], 2, sg['GroupId'])
+        create_instances(ec2_resource, instances_ami, "m4.large", private_key["KeyName"], "cluster2", subnets[0], 2, sg['GroupId'])
+
+        print("All instances created")
     except Exception as e:
         print(e)
 
-    awake = False
-    # for now number of instances are 1
     awake_instances = None
-    while awake is False:
+    # Wait for all instances to instanciate
+    while len(list(awake_instances.all())) != 9:
         # Name filter used to prevent existing instances from interfering
         awake_instances = ec2_resource.instances.filter(
             Filters=[
@@ -78,11 +68,10 @@ def main():
                 {'Name': 'tag:Name', 'Values': ['cluster1', 'cluster2']}
             ]
         )
-        if len(list(awake_instances.all())) == 9:
-            awake = True
 
     public_ips = [instance.public_ip_address for instance in awake_instances.all()]
 
+    # List of commands to run on the instances
     commands = [
         'chmod 777 install.sh',
         "./install.sh",
@@ -98,16 +87,19 @@ def main():
         "sudo sed -i 's/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 128;/g' /etc/nginx/nginx.conf",
         'sudo service nginx restart',
         'sudo apt install screen -y',
-        '/usr/bin/screen -dm gunicorn3 app:app']
+        '/usr/bin/screen -dm gunicorn3 app:app'
+    ]
+
     folder_path = os.path.abspath("flask_application")
-    files = [os.path.join(folder_path, "app.py"), os.path.join(folder_path, "nginxconfig"),
-             os.path.join(folder_path, "install.sh")]
+    files = [
+        os.path.join(folder_path, "app.py"),
+        os.path.join(folder_path, "nginxconfig"),
+        os.path.join(folder_path, "install.sh")
+    ]
 
     time.sleep(30)
+
     for ip in public_ips:
-        # thread = Thread(target=start_deployement, args=((ip, files, commands)))
-        # thread.start()
-        # threads.append(thread)
         start_deployement(ip, files, commands)
 
     cluster1_instances = awake_instances.filter(
@@ -126,14 +118,10 @@ def main():
     for ins in cluster2_instances.all():
         cluster2_targets_ids.append({'Id': ins.id})
 
-    threads = [Thread(target=add_instance_to_target_group, args=[elb_client, target_groups[0], cluster1_targets_ids]),
-               Thread(target=add_instance_to_target_group, args=[elb_client, target_groups[1], cluster2_targets_ids])]
-    #add_instance_to_target_group(
-     #   elb_client, target_groups[0], cluster1_targets_ids)
-
-#    add_instance_to_target_group(
- #       elb_client, target_groups[1], cluster2_targets_ids
-  #  )
+    threads = [
+        Thread(target=add_instance_to_target_group, args=[elb_client, target_groups[0], cluster1_targets_ids]),
+        Thread(target=add_instance_to_target_group, args=[elb_client, target_groups[1], cluster2_targets_ids])
+    ]
 
     for thread in threads:
         thread.start()
@@ -142,14 +130,14 @@ def main():
         thread.join()
 
     print("Set up completed")
-    # wait 30 seconds to simulate an interruption
 
-    # TODO: Modify this sleep
-    # time.sleep(60)
     load_balancer_dns = load_balancer["LoadBalancerDNS"]
-    # calling endpoints. TODO: Modify this code if needed
-    threads = [Thread(target=call_endpoint_http_thread1, args=[load_balancer_dns]),
-               Thread(target=call_endpoint_http_thread2, args=[load_balancer_dns])]
+
+    # sending requests to the endpoints.
+    threads = [
+        Thread(target=send_requests_thread1, args=[load_balancer_dns]),
+        Thread(target=send_requests_thread2, args=[load_balancer_dns])
+    ]
 
     for thread in threads:
         thread.start()
@@ -158,26 +146,22 @@ def main():
         thread.join()
 
     print("Requests were sent")
+
     # TODO: include cloud watch before tear down
+
     # Tear down
     print("Tearing down")
     delete_load_balancer(elb_client, load_balancer)
 
-    remove_instance_from_target_group(elb_client, target_groups[0],
-                                      cluster1_targets_ids)
+    remove_instance_from_target_group(elb_client, target_groups[0], cluster1_targets_ids)
+    remove_instance_from_target_group(elb_client, target_groups[1], cluster1_targets_ids)
 
-    remove_instance_from_target_group(elb_client, target_groups[1],
-                                      cluster1_targets_ids)
     for target_group in target_groups:
         delete_target_group(elb_client, target_group)
 
     terminate_instances(ec2_resource, cluster1_targets_ids)
     terminate_instances(ec2_resource, cluster2_targets_ids)
-    #time.sleep(5)
 
     delete_security_group(ec2_client, sg['GroupId'])
+
     print("Successfully deleted")
-
-
-
-main()

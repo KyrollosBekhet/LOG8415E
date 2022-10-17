e72fda12946d595eb5ef05db78f67019920613bd
David Piche <david.piche@polymtl.ca>
2022-10-17 17:28:38 -0400

    printPlot now aggregates all data in multiple y values for different instances
    Signed-off-by: David Piche <david.piche@polymtl.ca>

--- lab1/get_statistics.py
+++ lab1/get_statistics.py
@@ -7,18 +7,19 @@ from get_metrics import getMetric, getMetricInstance
 
 def getStatistics(session,instance_ids):
 
-
+    print(instance_ids)
+    
     client = session.client('cloudwatch')
 
     # Get CPU Usage on instances
     
     
-    
-    repCPU = getMetricInstance(client, instance_ids,'cpu_usage','AWS/EC2','CPUUtilization',30,'Sum','Percent',600)
-
-    print(repCPU)
-    
-
+    try:
+        repCPU = getMetricInstance(client, instance_ids,'cpu_usage','AWS/EC2','CPUUtilization',30,'Sum','Percent',600)
+        print(repCPU)
+        printPlot(repCPU, "test","CPU Utilization")
+    except Exception as e:
+        print(e)
     #resp = repCPU['MetricDataResults'][0]
 
 
@@ -33,17 +34,25 @@ def getStatistics(session,instance_ids):
     repTIME = getMetric(client, 'target_response_time', 'AWS/ApplicationELB','TargetResponseTime',60,'Average','Seconds',600)
     print(repTIME)
 
-def printPlot(resp, fileName):
+def printPlot(resp, fileName, ylabel):
 
-    y = np.array(resp['Values'][0])
-    x = np.array(resp['Timestamp'][0])
+    y = np.array(resp['MetricDataResults'][0]['Values'])
+    x = np.array(resp['MetricDataResults'][0]['Timestamp'])
 
 
-    for n in range(1,len(resp['Values'])):
+    #for n in range(1,len(resp['Values'])):
 
-        y = np.append(x,resp['Values'][n])
-        x = np.append(x,resp['Timestamp'][n])
+    #    y = np.append(x,resp['Values'][n])
+    #    x = np.append(x,resp['Timestamp'][n])
 
     plt.scatter(x, y)
+    
+    for r in range(1,resp['MetricDataResults']):
+        y1 = np.array(resp['MetricDataResults'][r]['Values'])
+        plt.scatter(x,y1)
+        
+    plt.xlabel("Timestamp")
+    plt.ylabel(ylabel)
+        
     plt.savefig(fileName)
 

c3db301f68b4daf037445663ffda8a7940aaed29
Philippe-Savard <65316883+Philippe-Savard@users.noreply.github.com>
2022-11-05 14:52:53 -0400

    Added time mesurements

--- lab2/spark/word_count.py
+++ lab2/spark/word_count.py
@@ -1,29 +1,40 @@
 import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import sum as _sum
 import os
 import urllib.request
+import time
 
 def generate_word_count(path, name, result_dir="results"):
     spark = SparkSession.builder.getOrCreate()
 
     # Create a DataFrame from the text file
     text_file = spark.sparkContext.textFile(path)
+    
+    start = time.time()
 
     # Run WordCount pipeline from https://spark.apache.org/examples.html
     counts = text_file.flatMap(lambda line: line.split(" ")) \
              .map(lambda word: (word, 1)) \
              .reduceByKey(lambda a, b: a + b)
 
-    # Create results destination directory
-    if not os.path.isdir(result_dir):
-            os.mkdir(result_dir)
+    end = time.time()
 
     # Convert pipeline back to DataFrame
     df_result = counts.toDF(["word", "count"])
-    df_result.show(5, truncate=False)
+
+    word_count = df_result.select(_sum('count')).alias('word_count').collect()[0][0]
+
+    print("MapReduce spent {} second(s) on file {} with {} words".format(end - start, name, word_count))
+
+    # Create results destination directory
+    if not os.path.isdir(result_dir):
+        os.mkdir(result_dir)
+
+    # counts.saveAsTextFile("{}/{}".format(result_dir, name))
 
     # Save dataframe to disk (or do something with it)
-    df_result.toPandas().to_csv("{}/{}.csv".format(result_dir, name))
+    df_result.toPandas().to_csv("{}/{}".format(result_dir, name), index=False)
 
     return df_result
 

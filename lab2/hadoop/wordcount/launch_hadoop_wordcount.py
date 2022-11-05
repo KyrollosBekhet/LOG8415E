88ad15af066debeafa180dedd2af1f1d3ac6970d
Mark Bekhet <markonsy72@gmail.com>
2022-11-05 17:41:39 -0400

    Write a python script to run hadoop wordcount
    Signed-off-by: Mark Bekhet <markonsy72@gmail.com>

---
+++ lab2/hadoop/wordcount/launch_hadoop_wordcount.py
@@ -0,0 +1,63 @@
+import os
+import urllib.request
+import time
+
+
+def run_word_count(path, name, try_number):
+    start = time.time()
+    os.system("hadoop jar wordcount.jar org.myorg.WordCount {0}/{1} output_{1}_{2}"
+              .format(path, name, try_number))
+    end = time.time()
+    return end - start
+
+
+def acquire_datasets(urls, dest_dir="text_datasets"):
+    filenames = []
+
+    # Create data destination directory
+    if not os.path.isdir(dest_dir):
+        os.mkdir(dest_dir)
+
+    for i, url in enumerate(urls):
+        filename = "text_{}".format(i)
+        filepath = "{}/{}".format(dest_dir, filename)
+
+        filenames.append((filepath, filename))
+
+        file = open(filepath, 'wb')
+
+        with urllib.request.urlopen(url) as response:
+            data = response.read()
+            file.write(data)
+
+        file.close()
+
+    return filenames
+
+
+def main():
+    os.system("export CLASSPATH=`hadoop classpath`:.:")
+    os.system("javac -d wordcount_classes WordCount.java")
+    os.system("jar -cvf wordcount.jar -C wordcount_classes/.")
+
+    urls = ['https://tinyurl.com/4vxdw3pa',
+            'https://tinyurl.com/kh9excea',
+            'https://tinyurl.com/dybs9bnk',
+            'https://tinyurl.com/datumz6m',
+            'https://tinyurl.com/j4j4xdw6',
+            'https://tinyurl.com/ym8s5fm4',
+            'https://tinyurl.com/2h6a75nk',
+            'https://tinyurl.com/vwvram8',
+            'https://tinyurl.com/weh83uyn'
+            ]
+
+    files = acquire_datasets(urls)
+
+    template_text = "the time required to analyze {} is {} \n"
+    final_text = ""
+    for i in range(0, 4):
+        for path, name in files:
+            time = run_word_count(path, name, i)
+            final_text += template_text.format(name, time)
+
+    print(final_text)
